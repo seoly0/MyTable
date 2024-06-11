76ddf379e6cc7963f68217c94e65705acbfab4ef
seoly0 <seoly0.dev@gmail.com>
2024-06-11 23:18:33 +0900

    add menu option relation jpa method

--- src/main/kotlin/me/seoly/goodtable/repository/MenuOptionRelationRepository.kt
+++ src/main/kotlin/me/seoly/goodtable/repository/MenuOptionRelationRepository.kt
@@ -1,7 +1,9 @@
 package me.seoly.goodtable.repository
 
 import me.seoly.goodtable.core.model.entity.MenuOptionRelation
+import me.seoly.goodtable.core.model.entity.OptionEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MenuOptionRelationRepository: JpaRepository<MenuOptionRelation, Long> {
+    fun findAllByStoreIdAndMenuId(storeId: Long, menuId: Long): List<MenuOptionRelation>
 }
